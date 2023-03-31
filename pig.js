9f79c446b3c3e985a6727f0eab37ba52743eca2c
Nellanlis <117353226+Nellanlis@users.noreply.github.com>
2023-03-31 17:26:25 +0200

    scalable pig

--- pig.js
+++ pig.js
@@ -14,42 +14,80 @@ function pig() {
   fill(253, 215, 228);
 
   //NOSE
-  push();
-  translate(pigX, pigY);
-  rotate(-0.8);
-  ellipse(-47, 180, 18, 28);
-  pop();
+  ellipse(pigX + 94 * pigS, pigY + 159 * pigS, 20 * pigS);
 
   //HEAD
-  ellipse(230, 324, 50);
+  ellipse(pigX + 80 * pigS, pigY + 174 * pigS, 50 * pigS);
 
   //EARS
   beginShape();
-  vertex(240, 310);
-  bezierVertex(250, 300, 250, 320, 255, 338);
-  bezierVertex(235, 330, 240, 325, 240, 320);
+  vertex(pigX + 90 * pigS, pigY + 160 * pigS);
+  bezierVertex(
+    pigX + 100 * pigS,
+    pigY + 150 * pigS,
+    pigX + 100 * pigS,
+    pigY + 170 * pigS,
+    pigX + 105 * pigS,
+    pigY + 188 * pigS
+  );
+  bezierVertex(
+    pigX + 85 * pigS,
+    pigY + 180 * pigS,
+    pigX + 90 * pigS,
+    pigY + 175 * pigS,
+    pigX + 90 * pigS,
+    pigY + 170 * pigS
+  );
   endShape();
   beginShape();
-  vertex(220, 305);
-  bezierVertex(215, 295, 205, 300, 200, 315);
-  bezierVertex(210, 314, 214, 312, 215, 307);
+  vertex(pigX + 70 * pigS, pigY + 155 * pigS);
+  bezierVertex(
+    pigX + 65 * pigS,
+    pigY + 145 * pigS,
+    pigX + 55 * pigS,
+    pigY + 150 * pigS,
+    pigX + 50 * pigS,
+    pigY + 165 * pigS
+  );
+  bezierVertex(
+    pigX + 60 * pigS,
+    pigY + 164 * pigS,
+    pigX + 64 * pigS,
+    pigY + 162 * pigS,
+    pigX + 65 * pigS,
+    pigY + 157 * pigS
+  );
   endShape();
 
   //BACK LEG
   beginShape();
-  vertex(178, 378);
-  bezierVertex(180, 390, 183, 393, 187, 395);
-  vertex(192, 390);
+  vertex(pigX + 28 * pigS, pigY + 228 * pigS);
+  bezierVertex(
+    pigX + 30 * pigS,
+    pigY + 240 * pigS,
+    pigX + 33 * pigS,
+    pigY + 243 * pigS,
+    pigX + 37 * pigS,
+    pigY + 245 * pigS
+  );
+  vertex(pigX + 42 * pigS, pigY + 240 * pigS);
   endShape();
   push();
   fill(139, 69, 19);
-  triangle(187, 395, 192, 390, 187, 384);
+  triangle(
+    pigX + 37 * pigS,
+    pigY + 245 * pigS,
+    pigX + 42 * pigS,
+    pigY + 240 * pigS,
+    pigX + 37 * pigS,
+    pigY + 234 * pigS
+  );
   pop();
 
   //BODY
   strokeWeight(1);
   push();
-  translate(pigX, pigY);
+  translate(pigX + pigS, pigY + pigS);
   rotate(0.5);
   ellipse(pigX + pigS, pigY + pigS, 70 * pigS, 80 * pigS);
   pop();
@@ -57,30 +95,88 @@ function pig() {
   //FRONT LEGS
   push();
   beginShape();
-  vertex(215, 385);
-  bezierVertex(215, 395, 225, 390, 235, 392);
-  vertex(240, 385);
-  bezierVertex(225, 385, 224, 375, 225, 378);
+  vertex(pigX + 65 * pigS, pigY + 235 * pigS);
+  bezierVertex(
+    pigX + 65 * pigS,
+    pigY + 245 * pigS,
+    pigX + 75 * pigS,
+    pigY + 240 * pigS,
+    pigX + 85 * pigS,
+    pigY + 242 * pigS
+  );
+  vertex(pigX + 90 * pigS, pigY + 235 * pigS);
+  bezierVertex(
+    pigX + 75 * pigS,
+    pigY + 235 * pigS,
+    pigX + 74 * pigS,
+    pigY + 225 * pigS,
+    pigX + 75 * pigS,
+    pigY + 228 * pigS
+  );
   endShape();
   beginShape();
-  vertex(236, 353);
-  bezierVertex(240, 360, 250, 360, 253, 360);
-  vertex(258, 352);
-  bezierVertex(246, 354, 247, 350, 245, 342);
+  vertex(pigX + 86 * pigS, pigY + 203 * pigS);
+  bezierVertex(
+    pigX + 90 * pigS,
+    pigY + 210 * pigS,
+    pigX + 100 * pigS,
+    pigY + 210 * pigS,
+    pigX + 103 * pigS,
+    pigY + 210 * pigS
+  );
+  vertex(pigX + 108 * pigS, pigY + 202 * pigS);
+  bezierVertex(
+    pigX + 96 * pigS,
+    pigY + 204 * pigS,
+    pigX + 97 * pigS,
+    pigY + 200 * pigS,
+    pigX + 95 * pigS,
+    pigY + 192 * pigS
+  );
   endShape();
   pop();
   push();
   fill(139, 69, 19);
-  triangle(253, 360, 258, 352, 253, 352);
-  triangle(235, 392, 240, 385, 235, 385);
+  triangle(
+    pigX + 103 * pigS,
+    pigY + 210 * pigS,
+    pigX + 108 * pigS,
+    pigY + 202 * pigS,
+    pigX + 103 * pigS,
+    pigY + 202 * pigS
+  );
+  triangle(
+    pigX + 85 * pigS,
+    pigY + 242 * pigS,
+    pigX + 90 * pigS,
+    pigY + 235 * pigS,
+    pigX + 85 * pigS,
+    pigY + 235 * pigS
+  );
   pop();
 
   //TAIL
   beginShape();
-  vertex(201, 365);
-  bezierVertex(190, 364, 190, 357, 195, 353);
-  bezierVertex(205, 354, 200, 365, 185, 363);
+  vertex(pigX + 51 * pigS, pigY + 215 * pigS);
+  bezierVertex(
+    pigX + 40 * pigS,
+    pigY + 214 * pigS,
+    pigX + 40 * pigS,
+    pigY + 207 * pigS,
+    pigX + 45 * pigS,
+    pigY + 203 * pigS
+  );
+  bezierVertex(
+    pigX + 55 * pigS,
+    pigY + 204 * pigS,
+    pigX + 50 * pigS,
+    pigY + 215 * pigS,
+    pigX + 35 * pigS,
+    pigY + 213 * pigS
+  );
   endShape();
 }
 
-pig();
+function draw() {
+  pig();
+}
