1a390d351d3aac975d9864dd6a8767991b818ea8
Natalie Lubinska <luna22id@student.ju.se>
2023-04-17 16:32:17 +0200

    linked "start game" to Gamescreen

--- startpage.js
+++ startpage.js
@@ -3,4 +3,5 @@ var canvas = document.getElementById("myCanvas");
 var button = document.getElementById("startButton");
 button.addEventListener("click", function () {
   // Code to execute when button is clicked
+  window.open("gamepage.html"); // Replace "other_page.html" with the URL of the page you want to open
 });
