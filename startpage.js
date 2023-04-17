f822f95450fba166270e93735c5c57707c8ee258
Natalie Lubinska <luna22id@student.ju.se>
2023-04-17 15:36:00 +0200

    start page started

---
+++ startpage.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("myCanvas");
+
+var button = document.getElementById("startButton");
+button.addEventListener("click", function () {
+  // Code to execute when button is clicked
+});
---
+++ tree.js
@@ -0,0 +1,55 @@
+let tractorX = 200;
+let tractorY = 200;
+let tractorS = 1;
+
+noStroke();
+
+fill(0, 255, 0);
+ellipse(
+  tractorX + 35 * tractorS,
+  tractorY + 35 * tractorS,
+  90 * tractorS,
+  90 * tractorS
+);
+
+fill(0, 255, 0);
+ellipse(
+  tractorX + 5 * tractorS,
+  tractorY + 78 * tractorS,
+  90 * tractorS,
+  86 * tractorS
+);
+
+fill(0, 255, 0);
+ellipse(
+  tractorX + 25 * tractorS,
+  tractorY + 120 * tractorS,
+  110 * tractorS,
+  70 * tractorS
+);
+
+fill(0, 255, 0);
+ellipse(
+  tractorX + 80 * tractorS,
+  tractorY + 105 * tractorS,
+  85 * tractorS,
+  85 * tractorS
+);
+
+fill(0, 255, 0);
+ellipse(
+  tractorX + 135 * tractorS,
+  tractorY + 100 * tractorS,
+  80 * tractorS,
+  70 * tractorS
+);
+
+fill(0, 255, 0);
+ellipse(
+  tractorX + 110 * tractorS,
+  tractorY + 45 * tractorS,
+  99 * tractorS,
+  80 * tractorS
+);
+
+
