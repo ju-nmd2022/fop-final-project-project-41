c9c1526eff7af81ba13d0a82c14307cac3487c39
Nellanlis <117353226+Nellanlis@users.noreply.github.com>
2023-04-24 17:55:26 +0200

    tree finished

--- tree.js
+++ tree.js
@@ -10,11 +10,26 @@ function tree() {
   stroke(0, 0, 0);
   strokeWeight(2);
   beginShape();
-  vertex(355, 400);
-  bezierVertex(380, 410, 420, 410, 445, 400);
-  bezierVertex(420, 340, 425, 260, 430, 220);
-  vertex(370, 220);
-  bezierVertex(375, 260, 380, 340, 355, 400);
+  vertex(380, 250);
+  bezierVertex(382, 297, 390, 340, 370, 360);
+  bezierVertex(390, 365, 410, 365, 430, 360);
+  bezierVertex(410, 340, 418, 297, 420, 250);
   endShape();
+  //LEAVES
+  fill(107, 142, 35);
+  ellipse(385, 140, 40);
+  ellipse(455, 205, 50);
+  ellipse(365, 230, 50);
+  ellipse(440, 240, 40);
+  ellipse(405, 240, 70);
+  ellipse(350, 195, 50);
+  ellipse(450, 175, 40);
+  ellipse(365, 155, 60);
+  ellipse(430, 140, 55);
+  /*ellipse(385, 195, 50);
+  ellipse(405, 165, 30);
+  ellipse(418, 192, 50); */
+  noStroke();
+  ellipse(400, 195, 100);
 }
 tree();
