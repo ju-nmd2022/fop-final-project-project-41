24f44110648766ddce9f1828a086e5d5eca43249
Nellanlis <117353226+Nellanlis@users.noreply.github.com>
2023-04-28 19:41:45 +0200

    hay moving

--- gamepage.js
+++ gamepage.js
@@ -3,7 +3,12 @@ background(107, 142, 35);
 let roadsX = 200;
 let roadsY = 200;
 
+let hayX = 600;
+let hayY = -438;
+let hayS = 1;
+
 function roads() {
+  push();
   stroke(244, 204, 140);
   strokeWeight(50);
   line(roadsX - 200, roadsY, roadsX + 400, roadsY + 100);
@@ -11,13 +16,40 @@ function roads() {
   line(roadsX - 200, roadsY - 400, roadsX + 400, roadsY - 300);
   stroke(135, 206, 250);
   line(roadsX - 200, roadsY - 600, roadsX + 400, roadsY - 500);
+  pop();
 }
-roads();
+
+function hay() {
+  // HAY FLOATING IN WATER
+  push();
+  fill(244, 204, 140);
+  strokeWeight(2);
+  rect(hayX + hayS, hayY + hayS, 200 * hayS, 100 * hayS, 10);
+  line(hayX + hayS, hayY + 85 * hayS, hayX + 200 * hayS, hayY + 85 * hayS);
+  pop();
+  //ropes
+  push();
+  stroke(139, 69, 19);
+  strokeWeight(10);
+  line(hayX + 60 * hayS, hayY + hayS, hayX + 60 * hayS, hayY + 85 * hayS);
+  line(hayX + 60 * hayS, hayY + 85 * hayS, hayX + 70 * hayS, hayY + 100 * hayS);
+  line(hayX + 140 * hayS, hayY + hayS, hayX + 140 * hayS, hayY + 85 * hayS);
+  line(
+    hayX + 140 * hayS,
+    hayY + 85 * hayS,
+    hayX + 150 * hayS,
+    hayY + 100 * hayS
+  );
+  pop();
+}
 
 function draw() {
   background(107, 142, 35);
   roads();
   roadsY += 2;
+  hay((hayS = 0.5), rotate(0.18));
+  hayY = hayY + 2;
+  hayX = hayX - 1;
 }
 
 /* if roads is equal to this y position I have to make it either stop. */
--- startpage.js
+++ startpage.js
@@ -6,7 +6,7 @@ button.addEventListener("click", function () {
   window.open("gamepage.html"); // Replace "other_page.html" with the URL of the page you want to open
 });
 
-/* addEventlistener at click for each of the animals and make the right one appear for the game screen 
+/* addEventlistener on click for each of the animals and make the right one appear for the game screen 
 and the right one for the ending text. Using if else statements. 
 We need to figure out a plan exactly in what order to code things so that it can all go together. 
 Let's start by picking your animal and getting them to jump in the correct way. Dont forget that we have to 
