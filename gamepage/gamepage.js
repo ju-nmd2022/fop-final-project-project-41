b501d058a41adf6d2ead5a9d1e54723efdf92d60
Natalie Lubinska <luna22id@student.ju.se>
2023-05-04 13:17:07 +0200

    made chicken move

--- gamepage/gamepage.js
+++ gamepage/gamepage.js
@@ -298,6 +298,204 @@ function tractor() {
   endShape(CLOSE);
 }
 
+/* if roads is equal to this y position I have to make it stop. */
+
+//trying to make chicken work
+
+let chickenX = 200;
+let chickenY = 200;
+let chickenS = 1;
+
+function chicken() {
+  fill(255, 255, 255);
+  strokeWeight(1);
+
+  //BEAK
+  push();
+  fill(255, 165, 0);
+  triangle(
+    chickenX + 100 * chickenS,
+    chickenY + 170 * chickenS,
+    chickenX + 108 * chickenS,
+    chickenY + 160 * chickenS,
+    chickenX + 95 * chickenS,
+    chickenY + 155 * chickenS
+  );
+  pop();
+
+  //HEAD
+  ellipse(chickenX + 85 * chickenS, chickenY + 169 * chickenS, 40 * chickenS);
+
+  //MOHAWK
+  push();
+  fill(255, 0, 0);
+  beginShape();
+  vertex(chickenX + 75 * chickenS, chickenY + 165 * chickenS);
+  bezierVertex(
+    chickenX + 65 * chickenS,
+    chickenY + 150 * chickenS,
+    chickenX + 70 * chickenS,
+    chickenY + 150 * chickenS,
+    chickenX + 75 * chickenS,
+    chickenY + 155 * chickenS
+  );
+  bezierVertex(
+    chickenX + 75 * chickenS,
+    chickenY + 145 * chickenS,
+    chickenX + 80 * chickenS,
+    chickenY + 145 * chickenS,
+    chickenX + 80 * chickenS,
+    chickenY + 155 * chickenS
+  );
+  bezierVertex(
+    chickenX + 80 * chickenS,
+    chickenY + 145 * chickenS,
+    chickenX + 85 * chickenS,
+    chickenY + 147 * chickenS,
+    chickenX + 85 * chickenS,
+    chickenY + 153 * chickenS
+  );
+  endShape(CLOSE);
+  pop();
+
+  //FEET
+  push();
+  fill(255, 165, 0);
+  ellipse(
+    chickenX + 70 * chickenS,
+    chickenY + 240 * chickenS,
+    25 * chickenS,
+    10 * chickenS
+  );
+  pop();
+
+  //BODY
+  push();
+  beginShape();
+  vertex(chickenX + 75 * chickenS, chickenY + 170 * chickenS);
+  bezierVertex(
+    chickenX + 40 * chickenS,
+    chickenY + 160 * chickenS,
+    chickenX + 5 * chickenS,
+    chickenY + 210 * chickenS,
+    chickenX + 39 * chickenS,
+    chickenY + 240 * chickenS
+  );
+  bezierVertex(
+    chickenX + 90 * chickenS,
+    chickenY + 260 * chickenS,
+    chickenX + 118 * chickenS,
+    chickenY + 190 * chickenS,
+    chickenX + 75 * chickenS,
+    chickenY + 170 * chickenS
+  );
+  endShape();
+  pop();
+
+  //WING ON SIDE
+  push();
+  fill(240, 240, 240);
+  beginShape();
+  vertex(chickenX + 85 * chickenS, chickenY + 220 * chickenS);
+  bezierVertex(
+    chickenX + 95 * chickenS,
+    chickenY + 190 * chickenS,
+    chickenX + 65 * chickenS,
+    chickenY + 190 * chickenS,
+    chickenX + 60 * chickenS,
+    chickenY + 210 * chickenS
+  );
+  bezierVertex(
+    chickenX + 57 * chickenS,
+    chickenY + 220 * chickenS,
+    chickenX + 65 * chickenS,
+    chickenY + 220 * chickenS,
+    chickenX + 68 * chickenS,
+    chickenY + 215 * chickenS
+  );
+  bezierVertex(
+    chickenX + 60 * chickenS,
+    chickenY + 225 * chickenS,
+    chickenX + 70 * chickenS,
+    chickenY + 225 * chickenS,
+    chickenX + 75 * chickenS,
+    chickenY + 215 * chickenS
+  );
+  bezierVertex(
+    chickenX + 72 * chickenS,
+    chickenY + 230 * chickenS,
+    chickenX + 85 * chickenS,
+    chickenY + 225 * chickenS,
+    chickenX + 85 * chickenS,
+    chickenY + 220 * chickenS
+  );
+  endShape();
+  pop();
+
+  //WING ON BUTT
+  push();
+  fill(240, 240, 240);
+  beginShape();
+  vertex(chickenX + 30 * chickenS, chickenY + 220 * chickenS);
+  bezierVertex(
+    chickenX + 10 * chickenS,
+    chickenY + 215 * chickenS,
+    chickenX + 15 * chickenS,
+    chickenY + 195 * chickenS,
+    chickenX + 25 * chickenS,
+    chickenY + 205 * chickenS
+  );
+  bezierVertex(
+    chickenX + 20 * chickenS,
+    chickenY + 190 * chickenS,
+    chickenX + 25 * chickenS,
+    chickenY + 185 * chickenS,
+    chickenX + 30 * chickenS,
+    chickenY + 200 * chickenS
+  );
+  bezierVertex(
+    chickenX + 30 * chickenS,
+    chickenY + 185 * chickenS,
+    chickenX + 35 * chickenS,
+    chickenY + 185 * chickenS,
+    chickenX + 37 * chickenS,
+    chickenY + 198 * chickenS
+  );
+  endShape(CLOSE);
+  pop();
+
+  if (keyIsDown(LEFT_ARROW)) {
+    chickenX -= 4;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    chickenX += 4;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    chickenY += -4;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    chickenY += 4;
+  }
+}
+function keyPressed(e) {
+  if (e.keyCode === 32) {
+    jump();
+  }
+}
+let targetChickenY;
+let velocity = 0;
+
+function jump() {
+  chickenY -= 100;
+  targetChickenY = chickenY + 100;
+  setTimeout(() => {
+    while (chickenY < targetChickenY) {
+      velocity += 0.1;
+      chickenY += velocity;
+    }
+  }, 1000);
+}
+
 function draw() {
   background(107, 142, 35);
   roads();
@@ -308,6 +506,5 @@ function draw() {
   tractor();
   tractorY = tractorY + 2;
   tractorX = tractorX - 3;
+  chicken();
 }
-
-/* if roads is equal to this y position I have to make it stop. */
