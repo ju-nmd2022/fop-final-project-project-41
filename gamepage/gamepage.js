53a0219286d9ef577e1eccb7e7a25682bc3db474
Nellanlis <117353226+Nellanlis@users.noreply.github.com>
2023-05-10 14:52:57 +0200

    more roads and barn

--- gamepage/gamepage.js
+++ gamepage/gamepage.js
@@ -11,6 +11,14 @@ let tractorX = 500;
 let tractorY = 110;
 let tractorS = 0.6;
 
+let chickenX = 200;
+let chickenY = 200;
+let chickenS = 1;
+
+let barnX = 300;
+let barnY = -1600;
+let barnS = 1;
+
 function roads() {
   push();
   stroke(244, 204, 140);
@@ -18,8 +26,12 @@ function roads() {
   line(roadsX - 200, roadsY, roadsX + 400, roadsY + 100);
   line(roadsX - 200, roadsY - 200, roadsX + 400, roadsY - 100);
   line(roadsX - 200, roadsY - 400, roadsX + 400, roadsY - 300);
+  line(roadsX - 200, roadsY - 800, roadsX + 400, roadsY - 700);
+  line(roadsX - 200, roadsY - 1400, roadsX + 400, roadsY - 1300);
   stroke(135, 206, 250);
   line(roadsX - 200, roadsY - 600, roadsX + 400, roadsY - 500);
+  line(roadsX - 200, roadsY - 1000, roadsX + 400, roadsY - 900);
+  line(roadsX - 200, roadsY - 1200, roadsX + 400, roadsY - 1100);
   pop();
 }
 
@@ -298,13 +310,7 @@ function tractor() {
   endShape(CLOSE);
 }
 
-/* if roads is equal to this y position I have to make it stop. */
-
 //trying to make chicken work
-
-let chickenX = 200;
-let chickenY = 200;
-let chickenS = 1;
 
 function chicken() {
   fill(255, 255, 255);
@@ -470,12 +476,105 @@ function chicken() {
   if (keyIsDown(RIGHT_ARROW)) {
     chickenX += 4;
   }
-  //if (keyIsDown(UP_ARROW)) {
-  // chickenY += -4;
-  //}
-  //if (keyIsDown(DOWN_ARROW)) {
-  //  chickenY += 4;
-  // }
+}
+
+function barn() {
+  push();
+  //GREY PART OF ROOF
+  fill(100, 100, 100);
+  beginShape();
+  vertex(barnX + 90 * barnS, barnY - 85 * barnS);
+  vertex(barnX + 170 * barnS, barnY - 83 * barnS);
+  vertex(barnX + 200 * barnS, barnY - 55 * barnS);
+  vertex(barnX + 217 * barnS, barnY - 20 * barnS);
+  vertex(barnX + 120 * barnS, barnY + barnS);
+  endShape(CLOSE);
+
+  //RED PARTS
+  fill(255, 0, 0);
+  strokeWeight(2);
+  beginShape();
+  vertex(barnX + barnS, barnY + barnS);
+  vertex(barnX + 10 * barnS, barnY + 50 * barnS);
+  vertex(barnX + 130 * barnS, barnY + 60 * barnS);
+  vertex(barnX + 145 * barnS, barnY - 10 * barnS);
+  bezierVertex(
+    barnX + 100 * barnS,
+    barnY - 130 * barnS,
+    barnX + 15 * barnS,
+    barnY - 90 * barnS,
+    barnX + barnS,
+    barnY + barnS
+  );
+  endShape();
+  beginShape();
+  vertex(barnX + 130 * barnS, barnY + 60 * barnS);
+  vertex(barnX + 210 * barnS, barnY + 45 * barnS);
+  vertex(barnX + 215 * barnS, barnY - 15 * barnS);
+  vertex(barnX + 145 * barnS, barnY - 10 * barnS);
+  endShape();
+
+  // WHITE PART OF ROOF
+  fill(255, 255, 255);
+  strokeWeight(1);
+  beginShape();
+  vertex(barnX + 217 * barnS, barnY - 13 * barnS);
+  vertex(barnX + 145 * barnS, barnY - 8 * barnS);
+  vertex(barnX + 120 * barnS, barnY - 55 * barnS);
+  vertex(barnX + 90 * barnS, barnY - 80 * barnS);
+  vertex(barnX + 50 * barnS, barnY - 80 * barnS);
+  vertex(barnX + 30 * barnS, barnY - 60 * barnS);
+  vertex(barnX + barnS, barnY + barnS);
+  vertex(barnX - 5 * barnS, barnY - 5 * barnS);
+  vertex(barnX + 25 * barnS, barnY - 65 * barnS);
+  vertex(barnX + 45 * barnS, barnY - 86 * barnS);
+  vertex(barnX + 95 * barnS, barnY - 86 * barnS);
+  vertex(barnX + 128 * barnS, barnY - 57 * barnS);
+  vertex(barnX + 149 * barnS, barnY - 15 * barnS);
+  vertex(barnX + 217 * barnS, barnY - 20 * barnS);
+  endShape(CLOSE);
+
+  //WINDOWS
+  fill(139, 69, 19);
+  stroke(255, 255, 255);
+  strokeWeight(4);
+  rect(barnX + 55 * barnS, barnY - 45 * barnS, 30 * barnS, 30 * barnS);
+  rect(barnX + 155 * barnS, barnY + barnS, 15 * barnS, 25 * barnS);
+  rect(barnX + 180 * barnS, barnY - 2 * barnS, 15 * barnS, 25 * barnS);
+  beginShape();
+  vertex(barnX + 40 * barnS, barnY + 53 * barnS);
+  vertex(barnX + 40 * barnS, barnY + 10 * barnS);
+  vertex(barnX + 90 * barnS, barnY + 12 * barnS);
+  vertex(barnX + 85 * barnS, barnY + 56 * barnS);
+  endShape();
+  line(
+    barnX + 40 * barnS,
+    barnY + 10 * barnS,
+    barnX + 85 * barnS,
+    barnY + 55 * barnS
+  );
+  line(
+    barnX + 90 * barnS,
+    barnY + 12 * barnS,
+    barnX + 40 * barnS,
+    barnY + 55 * barnS
+  );
+  line(
+    barnX + 155 * barnS,
+    barnY + barnS,
+    barnX + 170 * barnS,
+    barnY + 25 * barnS
+  );
+  line(
+    barnX + 180 * barnS,
+    barnY + barnS,
+    barnX + 193 * barnS,
+    barnY + 20 * barnS
+  );
+  fill(244, 204, 140);
+  noStroke();
+  rect(barnX + 57 * barnS, barnY - 30 * barnS, 26 * barnS, 13 * barnS);
+  pop();
 }
 
 function keyPressed(e) {
@@ -484,7 +583,8 @@ function keyPressed(e) {
   }
 }
 let targetChickenY;
-let velocity = 3;
+let velocity = 1;
+let acceleration = 0.1;
 
 function jump() {
   chickenY -= 100;
@@ -495,7 +595,7 @@ function jump() {
       chickenY += velocity;
     }
   }, 100);
-  targetChickenY -= 45;
+  targetChickenY -= 51;
 }
 
 function draw() {
@@ -510,6 +610,8 @@ function draw() {
   tractorX = tractorX - 3;
   chicken();
   chickenY = chickenY + 1;
+  barn(rotate(-0.1));
+  barnY = barnY + 1;
 
   if ((y = 530)) {
   }
